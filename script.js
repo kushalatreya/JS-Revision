cfe1633beae39e37472660738ebd062a384e2b85
kushal atreya <kushalatreya@live.com>
2018-03-06 09:47:23 +0200

    arrays and more ways of arrays

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 //This will be the script
-//case sensitive
+/*
+//1.case sensitive
 var test = "This is a test"
 var Test = "This is another test"
-
 // Test would overwrite test, if var was not case-sensitive.
-
 alert(test);
+*/
+//2.Arrays
+
+var food = ["momo", "pizza", "burger"];
+alert(food[1]);
+//this will start numbering from 0,1,2 so it will show pizza for [1]
+
+//another way to Arrays
+
+var Food = new Array("Momo", "Pizza", "Burger");
+//adding the Arrays
+Food[3] = "moreMomo"
+alert(Food[3]);
