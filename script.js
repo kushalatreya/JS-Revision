19e10adf37913df595c7efd43258e6941ad36fbd
kushal atreya <kushalatreya@live.com>
2018-03-06 10:01:10 +0200

    arrays and more ways of arrays

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ var test = "This is a test"
 var Test = "This is another test"
 // Test would overwrite test, if var was not case-sensitive.
 alert(test);
-*/
+
 //2.Arrays
 
 var food = ["momo", "pizza", "burger"];
-alert(food[1]);
-//this will start numbering from 0,1,2 so it will show pizza for [1]
+alert(food[1]);//this will start numbering from 0,1,2 so it will show pizza for [1]
 
 //another way to Arrays
 
-var Food = new Array("Momo", "Pizza", "Burger");
-//adding the Arrays
-Food[3] = "moreMomo"
-alert(Food[3]);
+var Food = new Array("Momo", "Pizza", "Burger");//adding the Arrays
+//Food[3] = "moreMomo";
+Food.push("more Momo");//methods
+alert(Food[3] + " and " +Food[1]);
+*/
+var numbers = [0,5,7,1,3];
+alert(numbers[1] + numbers[3]);
+alert(numbers.length);//this will show the length
+alert(numbers.sort());//this will sort the numbers
