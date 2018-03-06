29f3d44e6bb86b37291bbee1e6ba332ce414e235
kushal atreya <kushalatreya@live.com>
2018-03-06 09:32:09 +0200

    Variables and case sensitive

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //This will be the script
+//case sensitive
+var test = "This is a test"
+var Test = "This is another test"
+
+// Test would overwrite test, if var was not case-sensitive.
+
+alert(test);
